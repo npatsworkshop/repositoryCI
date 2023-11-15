3e18243d183ad8ec0996413241a310e1068f12a6
npatsworkshop <150718451+npatsworkshop@users.noreply.github.com>
2023-11-15 15:37:41 +0100

    Update example.py

--- example.py
+++ example.py
@@ -3,3 +3,7 @@ def add(a, b):
 
 def subtract(a, b):
     return a + b  # do not change this line until prompted to do so.
+
+def test_add():
+    assert add(5, 5) == 10 
+    assert add(-5, 5) == 0
